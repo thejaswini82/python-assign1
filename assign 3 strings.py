26304ede6bbc935994a4ccdb86983420d76f001e
THEJASWINI <113339232+thejaswini82@users.noreply.github.com>
2023-05-18 23:34:14 +0530

    Add files via upload

---
+++ assign 3 strings.py
@@ -0,0 +1,7 @@
+def reverse_string():
+    string = input("Enter a string: ")
+    reversed_string = string[::-1]
+    return reversed_string
+
+result = reverse_string()
+print("Reversed string:", result)
